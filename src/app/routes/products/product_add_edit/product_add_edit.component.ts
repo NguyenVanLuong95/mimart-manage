166a04c87eb8666fee1053bc3562fd5f009ccfb2
NguyenVanLuong95 <nguyenluong040595@gmail.com>
2021-09-12 17:17:05 +0700

    update code

--- src/app/routes/products/product_add_edit/product_add_edit.component.ts
+++ src/app/routes/products/product_add_edit/product_add_edit.component.ts
@@ -24,6 +24,7 @@ export class ProductAddEditComponent implements OnInit {
   ngOnInit() {
     this.addProductForm = this.fb.group({
       productName: ['', Validators.required],
+      unitPrice: ['', Validators.required],
       productImage: ['']
     });
   }
@@ -35,21 +36,23 @@ export class ProductAddEditComponent implements OnInit {
   onChangeFileInput(): void {
     const files: { [key: string]: File } = this.fileInput.nativeElement.files;
     this.file = files[0];
-    this.addProductForm.controls.categorImage.setValue(this.file.name ? this.file.name : '')
+    this.addProductForm.controls.productImage.setValue(this.file.name ? this.file.name : '')
   }
   onClickFileInputButton(): void {
     this.fileInput.nativeElement.click();
   }
   onSave() {
     const formData = new FormData();
-    formData.append('categoryImage', this.file);
-    formData.append('categoryName', this.addProductForm.controls.productName.value);
-    // this.productAddEditService.onSave(formData).subscribe(res => {
-    //   if (res.code == 1) {
-    //     this.toastr.success("Thêm mới sản phẩm thành công!");
-    //   } else {
-    //     this.toastr.error("Thêm mới sản phẩm thất bại!")
-    //   }
-    // });
+    formData.append('categoryId', this.data.record)
+    formData.append('productImage', this.file);
+    formData.append('productName', this.addProductForm.controls.productName.value);
+    formData.append('unitPrice', this.addProductForm.controls.unitPrice.value);
+    this.productAddEditService.onSave(formData).subscribe(res => {
+      if (res.code == 1) {
+        this.toastr.success("Thêm mới sản phẩm thành công!");
+      } else {
+        this.toastr.error("Thêm mới sản phẩm thất bại!")
+      }
+    });
   }
 }
