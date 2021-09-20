1781b187b707388df51a3c7e1e9858cb55ea4970
DESKTOP-54CULAG\ADMIN <luongnv15@viettel.com.vn>
2021-09-20 22:44:56 +0700

    update edit user

---
+++ src/app/routes/users/user_add_edit/user_add_edit.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { baseUrl } from '@shared/constant';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class UserAddEditService {
+  constructor(private http: HttpClient) { }
+  onSave(body): Observable<any> {
+    return this.http.post<any>(`${baseUrl.baseUrl}/api/users`, body);
+  }
+}
