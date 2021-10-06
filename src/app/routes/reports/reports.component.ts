402f88a6be82b8e583d3689390550eba5b16c1f7
DESKTOP-54CULAG\ADMIN <luongnv15@viettel.com.vn>
2021-10-06 21:42:00 +0700

    update code

--- src/app/routes/reports/reports.component.ts
+++ src/app/routes/reports/reports.component.ts
@@ -1,9 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReportsService } from './reports.service';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
 })
 export class ReportsComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void {}
+  listStories: any;
+  listReport: any;
+  reportForm = new FormGroup({});
+  constructor(
+    private reportService: ReportsService,
+    private fb: FormBuilder
+  ) { }
+  ngOnInit(): void {
+    this.reportForm = this.fb.group({
+      story: ['', Validators.required],
+    });
+    this.getListStories();
+    this.reportForm.controls['story'].setValue(1);
+    this.getReport()
+  }
+
+  getListStories() {
+    this.reportService.getListStories().subscribe(res => {
+      this.listStories = res
+    })
+  }
+  getReport() {
+    this.reportService.getReport(this.reportForm.controls['story'].value).subscribe(res => {
+      this.listReport = res.content;
+    })
+  }
 }
