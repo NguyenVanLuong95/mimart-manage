06b1b48a527dd3eede1ae65281186516d18881d7
DESKTOP-54CULAG\ADMIN <luongnv15@viettel.com.vn>
2021-10-15 15:35:40 +0700

    update code

--- src/app/routes/reports/reports.component.ts
+++ src/app/routes/reports/reports.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReportsService } from './reports.service';
+import { Moment } from 'moment';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
@@ -13,7 +15,8 @@ export class ReportsComponent implements OnInit {
   displayedColumns = ['productName', 'unitPrice', 'inventoryQuantity', 'discount', 'soldQuantity', 'revenue'];
   constructor(
     private reportService: ReportsService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private toastr: ToastrService
   ) { }
   ngOnInit(): void {
     this.reportForm = this.fb.group({
@@ -35,9 +38,33 @@ export class ReportsComponent implements OnInit {
   }
   getReport() {
     let storyId = this.reportForm.controls['story'].value;
-    let dateFrom = this.reportForm.controls['dateFrom'].value;
-    let dateTo = this.reportForm.controls['dateTo'].value;
-    this.reportService.getReport(storyId, dateFrom, dateTo).subscribe(res => {
+    let timeDateFrom: Moment = this.reportForm.controls['dateFrom'].value;
+    let yearDateFrom = timeDateFrom.toObject().years;
+    let monthDateFrom = timeDateFrom.toObject().months + 1;
+    let dayDateFrom = timeDateFrom.toObject().date;
+    let dateFrom = yearDateFrom + '-' + monthDateFrom + '-' + dayDateFrom;
+    let timeDateTo: Moment = this.reportForm.controls['dateTo'].value;
+    let yearDateTo = timeDateTo.toObject().years;
+    let monthDateTo = timeDateTo.toObject().months + 1;
+    let dayDateTo = timeDateTo.toObject().date;
+    let dateTo = yearDateTo + '-' + monthDateTo + '-' + dayDateTo;
+    if (yearDateFrom > yearDateTo) {
+      this.toastr.error("Thời gian từ ngày không được lớn hơn đến ngày!");
+      return;
+    }
+    if ((yearDateFrom == yearDateTo) && (monthDateFrom > monthDateTo)) {
+      this.toastr.error("Thời gian từ ngày không được lớn hơn đến ngày!");
+      return;
+    }
+    if ((yearDateFrom == yearDateTo) && (monthDateFrom == monthDateTo) && (dayDateFrom > dayDateTo)) {
+      this.toastr.error("Thời gian từ ngày không được lớn hơn đến ngày!");
+      return;
+    }
+    let params = {
+      dateFrom: dateFrom,
+      dateTo: dateTo
+    }
+    this.reportService.getReport(storyId, params).subscribe(res => {
       this.listReport = res.content;
     })
   }
