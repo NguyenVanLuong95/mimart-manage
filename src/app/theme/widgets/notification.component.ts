7f721739b73d20395e500614cab5df4e39d292b8
NguyenVanLuong95 <nguyenluong040595@gmail.com>
2021-11-07 09:42:55 +0700

    update code

--- src/app/theme/widgets/notification.component.ts
+++ src/app/theme/widgets/notification.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OrdersService } from 'app/routes/orders/orders.service';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-notification',
   template: `
+    <button mat-icon-button matTooltip="Nhấp để xem mã đơn hàng mới đặt" class="matero-toolbar-button" [matMenuTriggerFor]="menu">
+      <mat-icon>notifications</mat-icon>
+      <span class="badge bg-red-500">{{ count }}</span>
+    </button>
+
+    <mat-menu #menu="matMenu">
+      <mat-nav-list>
+        <mat-list-item *ngFor="let message of messages">
+          <a matLine>{{ message }}</a>
+          <button mat-icon-button>
+            <mat-icon>info</mat-icon>
+          </button>
+        </mat-list-item>
+      </mat-nav-list>
+    </mat-menu>
   `,
 })
 export class NotificationComponent {
-  messages = ['Server Error Reports', 'Server Error Reports', 'Server Error Reports'];
+  messages = [];
+  count!: number;
+  minutes!: number;
+  @Input()
+  intervalPeriod!: number;
+  subscription!: Subscription;
+  constructor(private orderService: OrdersService) { }
+  ngOnInit(): void {
+    this.subscription = interval(30000).subscribe((x => {
+      this.getNotifications();
+    }));
+  }
+
+  getNotifications() {
+    this.orderService.getListNewOrders().subscribe(res => {
+      this.count = res.content.length;
+      this.messages = res.content.map(x => x.orderCode);
+    })
+  }
 }
